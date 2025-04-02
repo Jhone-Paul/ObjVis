8f5c517a0943d97280f5fd91970d2194a4747454
johnleithpowell <johnleithpowell@gmail.com>
2025-04-02 21:26:56 +0200

    added a model class wutg triangles and verticies

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class DemoViewer {
   public static void main(String[] args) {
@@ -8,7 +9,8 @@ public class DemoViewer {
       FilePath = args[0];
       System.out.println("Showing files at: "+ FilePath);
     } catch (Exception e) {
-      System.out.println("Error, please ensure the right amount of arguments \n" + e);
+      System.err.println("Error, please ensure the right amount of arguments \n" + e);
+      System.exit(0);
     }
 
     JFrame frame = new JFrame("OBJ Viewer");
@@ -36,4 +38,62 @@ public class DemoViewer {
       frame.setVisible(true);
   }
 }
+
+// Vector3D class for 3D vectors
+class Vector3D {
+    public double x, y, z;
+    
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public Vector3D subtract(Vector3D other) {
+        return new Vector3D(x - other.x, y - other.y, z - other.z);
+    }
+    
+    public Vector3D cross(Vector3D other) {
+        return new Vector3D(
+            y * other.z - z * other.y,
+            z * other.x - x * other.z,
+            x * other.y - y * other.x
+        );
+    }
+    
+    public double dot(Vector3D other) {
+        return x * other.x + y * other.y + z * other.z;
+    }
+    
+    public double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+    
+    public Vector3D normalize() {
+        double len = length();
+        if (len > 0) {
+            return new Vector3D(x / len, y / len, z / len);
+        }
+        return new Vector3D(0, 0, 0);
+    }
+}
+
+// Triangle class for faces
+class Triangle {
+    public int v1, v2, v3; // Indices into the vertex list
+    
+    public Triangle(int v1, int v2, int v3) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+    }
+}
+
+// Model class to hold the OBJ data
+class Model {
+    public ArrayList<Vector3D> vertices = new ArrayList<>();
+    public ArrayList<Triangle> triangles = new ArrayList<>();
+}
+
+
 
