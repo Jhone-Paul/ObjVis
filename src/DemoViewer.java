20a1c152688c48b0e96143ba539888a23b717525
johnleithpowell <johnleithpowell@gmail.com>
2025-04-02 21:05:17 +0200

    started the frame for the visualiser

---
+++ src/DemoViewer.java
@@ -0,0 +1,28 @@
+public public class DemoViewer {
+  public static void main()String[] args {
+    JFrame frame = new JFrame("OBJ Viewer");
+    frame.setDefualtCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Container pane = frame.getContentPane();
+    pane.setLayout(new BorderLayout());
+  
+    JSlider headingSlider = new JSlider(0,360,180);
+    pane.add(headingSlider, BorderLayout.SOUTH);
+
+    JSlider pitchSlider = new JSlider(0,360,180);
+    pane.add(pitchSlider, BorderLayout.EAST);
+    JPanel renderPanel = new JPanel() {
+      public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, getWidth(), getHeight());
+
+                    // rendering magic will happen here
+        }
+      };
+      pane.add(renderPanel, BorderLayout.CENTER);
+
+      frame.setSize(400, 400);
+      frame.setVisible(true);
+  }
+}
+
